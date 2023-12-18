65513ca8c521bf59155ff4ca3da85453a948ff46
taitrongnguyen <luongnguyen@Luongs-MacBook-Air.local>
2023-12-18 17:20:15 +0700

    Update code oAuth

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+  return (
+    <>
+      <div>Home</div>
+      <button onClick={logout}>Log out</button>
+    </>
+  );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,29 +1,42 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Link } from "react-router-dom";
-
-const getGoogleAuthUrl = () => {
-  const url = "https://accounts.google.com/o/oauth2/v2/auth";
-  const { VITE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env;
-  console.log(import.meta.env);
-  const query = {
-    client_id: VITE_CLIENT_ID,
-    redirect_uri: VITE_GOOGLE_REDIRECT_URI,
-    response_type: "code",
-    scope: [
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email",
-    ].join(" "),
-    prompt: "consent",
-    access_type: "offline",
-  };
-  const queryString = new URLSearchParams(query).toString();
-  return `${url}?${queryString}`;
-};
-const googleOauthURL = getGoogleAuthUrl();
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function Login() {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+  const getGoogleAuthUrl = () => {
+    const url = "https://accounts.google.com/o/oauth2/v2/auth";
+    const { VITE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env;
+    const query = {
+      client_id: VITE_CLIENT_ID,
+      redirect_uri: VITE_GOOGLE_REDIRECT_URI,
+      response_type: "code",
+      scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+      ].join(" "),
+      prompt: "consent",
+      access_type: "offline",
+    };
+    const queryString = new URLSearchParams(query).toString();
+    return `${url}?${queryString}`;
+  };
+  const googleOauthURL = getGoogleAuthUrl();
+  useEffect(() => {
+    if(params) {
+      const accessToken = params.get("access_token");
+      const refreshToken = params.get("refresh_token");
+      if (accessToken && refreshToken) {
+        localStorage.setItem("access_token", accessToken);
+        localStorage.setItem("refresh_token", refreshToken);
+        navigate('/')
+      }
+    }
+  }, [params, navigate]);
+
   return (
     <>
       <div>
